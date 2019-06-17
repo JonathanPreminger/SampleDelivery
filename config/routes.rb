ee8f9f38f357d9d029fb18b73250e066fb800456
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-06-17 14:04:53 +0200

    modifying routes and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   get 'events/index'
-  get 'events/import'
-  get 'events/edit'
-  get 'events/update'
-  get 'events/destroy'
-  get 'events/event_params'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
