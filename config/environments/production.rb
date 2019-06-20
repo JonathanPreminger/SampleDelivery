e8bda04a89d6792f53cd9a129eb33551965971e8
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-06-21 00:33:51 +0200

    change production.rb and app.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
   config.assets.compile = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.digest = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
