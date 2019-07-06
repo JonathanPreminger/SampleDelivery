8442a820b6d235ea4e1d4debb14792b3e122c674
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-07-06 02:35:41 +0200

    Add foreign key to Realrelease to artist

--- app/models/realrelease.rb
+++ app/models/realrelease.rb
@@ -1,4 +1,4 @@
 class Realrelease < ApplicationRecord
-  belongs_to :artist, inverse_of: :realreleases
+  belongs_to :artist,:foreign_key => "creator_id", inverse_of: :realreleases
   has_many :tracks
 end
