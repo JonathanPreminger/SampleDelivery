f294b45912b3f21ecc68649ccacce7e972964d5e
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-07-07 13:12:25 +0200

    fixin definitively Tracks issue

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :realreleases, dependent: :destroy, inverse_of: :artist
-  has_many :tracks, through: :realreleases
-  accepts_nested_attributes_for :realreleases,  reject_if: proc { |attributes| attributes[:name].blank? }, allow_destroy: true
+  has_many :realreleases
+  has_many :tracks, through: :realrelease
+  accepts_nested_attributes_for :realreleases
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,4 @@
 class Track < ApplicationRecord
+  belongs_to :realrelease, inverse_of: :tracks
+  validates_presence_of :realrelease
 end
