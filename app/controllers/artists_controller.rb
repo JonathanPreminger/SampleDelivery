9cd03e454cd18bc200d5b372a81a8b3ce67db17f
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-07-12 07:57:31 +0200

    add css and add image attached to artist model

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -49,6 +49,6 @@ end
   private
 
   def artist_params
-    params.require(:artist).permit(:name, realreleases_attributes: [:id, :name, :_destroy])
+    params.require(:artist).permit(:name, :image, realreleases_attributes: [:id, :name, :year, :_destroy])
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,5 @@ class Artist < ApplicationRecord
   has_many :realreleases
   has_many :tracks, through: :realrelease
   accepts_nested_attributes_for :realreleases
+  has_one_attached :image
 end
