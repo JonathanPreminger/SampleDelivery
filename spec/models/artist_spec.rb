ac97a50984236bbdb95e718b9edab6628f9ab8f5
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-09-05 10:55:04 +0200

    add test on model djset and artist

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Artist, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Artist, :type => :model do
+  it "is valid with valid attributes" do
+    expect(Artist.new).to be_valid
+end
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:name).of_type(:text)}
+  end
+
+  describe 'Validations' do
+  #  it { is_expected.to validate_presence_of(:artist) }
+  #  it { is_expected.to validate_presence_of(:club) }
+  #  it { is_expected.to validate_presence_of(:start) }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to have_many(:realreleases).dependent(:destroy) }
+    it { is_expected.to have_many(:djsets).dependent(:destroy) }
+    it { should have_many(:tracks).through(:realreleases).dependent(:destroy) } 
+
+
+  end
 end
--- spec/models/djset_spec.rb
+++ spec/models/djset_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Djset, type: :model do
-  #pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Djset, :type => :model do
+  it "is valid with valid attributes" do
+    expect(Djset.new).not_to be_valid
+end
+
+  describe 'Database' do
+    it { is_expected.to have_db_column(:id).of_type(:integer) }
+    it { is_expected.to have_db_column(:start).of_type(:datetime)}
+    it { is_expected.to have_db_column(:club).of_type(:text) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:artist) }
+    it { is_expected.to validate_presence_of(:club) }
+    it { is_expected.to validate_presence_of(:start) }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to(:artist) }
+
+  end
 end
