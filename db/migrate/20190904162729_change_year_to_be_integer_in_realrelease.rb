465966f8a1d31a1ad04997cf5b95cc72183f3561
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-09-10 11:44:22 +0200

    trying to solve a changing type issue when pushing to heroku

--- db/migrate/20190904162729_change_year_to_be_integer_in_realrelease.rb
+++ db/migrate/20190904162729_change_year_to_be_integer_in_realrelease.rb
@@ -1,5 +1,5 @@
 class ChangeYearToBeIntegerInRealrelease < ActiveRecord::Migration[5.2]
   def change
-      change_column :realreleases, :year, :integer
+      change_column :realreleases, :year, :integer, using: 'year::integer'
   end
 end
