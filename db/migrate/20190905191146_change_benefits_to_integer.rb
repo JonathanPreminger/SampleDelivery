fd489326b54e7f438149bcfae9ecb2d0b87ee311
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-09-10 11:49:15 +0200

    almost solving the changing type issue

--- db/migrate/20190905191146_change_benefits_to_integer.rb
+++ db/migrate/20190905191146_change_benefits_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeBenefitsToInteger < ActiveRecord::Migration[5.2]
   def change
-    change_column :events, :benefits, :integer
+    change_column :events, :benefits, :integer, using: 'benefits::integer'
   end
 end
