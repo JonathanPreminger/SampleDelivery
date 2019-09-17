5fcf843fc04751da753f8510335f83aff91d84bc
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-09-17 16:17:32 +0200

    changing config production mailer smtp.rb with sendgrid encore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,16 @@
   config.active_record.dump_schema_after_migration = false
 
 
-config.action_mailer.default_url_options = { :host => 'sampledeliveryrecords.herokuapp.com' }
+config.action_mailer.default_url_options = { :host => 'http://sampledeliveryrecords.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
-
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address:              'smtp.sendgrid.net',
+  port:                  485,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  authentication:       :login,
+  enable_starttls_auto: true  }
 end
