ea348f1516e03eed1533e7070f47999d3bdb94b4
JonathanPreminger <jonathan.mete@hotmail.fr>
2019-09-17 16:04:58 +0200

    changing config production mailer smtp.rb with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@
   # config.active_job.queue_name_prefix = "SampleDelivery_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.active_job.queue_adapter = :async
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -98,13 +98,5 @@ config.action_mailer.default_url_options = { :host => 'sampledeliveryrecords.her
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = false
-config.action_mailer.default :charset => "utf-8"
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            ENV["GMAIL_EMAIL"],
-  password:             ENV["GMAIL_PASSWORD"],
-  authentication:       :login,
-  enable_starttls_auto: true  }
+
 end
